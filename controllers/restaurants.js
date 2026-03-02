2b3b2ed376dd3c43bff5ac71035510fd9701e8b8
parkarov <keerati2904@gmail.com>
2026-03-02 23:37:01 +0700

    changing status on getRestaurant

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -80,12 +80,12 @@ exports.getRestaurant= async (req,res,next)=>{
         const restaurant = await Restaurant.findById(req.params.id);
 
         if (!restaurant) {
-            return res.status (400).json({success:false});
+            return res.status (404).json({success:false});
         }
 
         res.status (200).json({success:true, data:restaurant});
     } catch(err){
-        res.status (400).json({success:false});
+        res.status (500).json({success:false});
     }
 };
 
